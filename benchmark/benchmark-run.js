55b783b210cacf7c3f4f110045760ee5cf823792
Lassalle Alexandre <alexandrelassalle@gmail.com>
2022-12-27 20:35:00 +0100

    autocannon

--- benchmark/benchmark-run.js
+++ benchmark/benchmark-run.js
@@ -1,4 +1,80 @@
 const autocannon = require('autocannon');
+const fs = require('fs');
+
+const RESULT_PATH = './result_autocannon.csv';
+fs.writeFileSync(RESULT_PATH, '');
+
+function constructPayload() {
+  return {
+    url: 'http://localhost:3001/youpi/short/',
+    created_at: '2022-12-27T19:07:53.585Z',
+    modified_by: 'alexandre',
+  };
+}
+
+/**
+ * Construct an AutoCannon instance.
+ * Play around with the 'amount' and 'overallRate' values.
+ *
+ * @param {string} title  The string title of this instance
+ * @param {string} url    The string target URL for this instance
+ */
+function constructAutoCannonInstance(title, url) {
+  return autocannon(
+    {
+      title,
+      url,
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify({
+        data: constructPayload(),
+      }),
+      connections: 10, // ITU-T suggests using 10 gateways (concurent connection)
+      pipelining: 1, // default
+      bailout: 10000, // tolerable number of errors
+      // overallRate: 6000, // rate of requests to make per second from all connections
+      amount: 1500, // ITU-T suggests 15,000,000 IoT requests per day
+      duration: 1,
+    },
+    console.log,
+  );
+}
+
+function startBench(path) {
+  const instance = constructAutoCannonInstance('yoyo', path);
+
+  autocannon.track(instance, {
+    renderProgressBar: true,
+    renderResultsTable: true,
+    renderLatencyTable: false,
+    progressBarString: 'Running :percent | Elapsed :elapsed (seconds) | Rate :rate | ETA :eta (seconds)',
+  });
+
+  instance.on('tick', (counter) => {
+    if (counter.counter === 0) {
+      console.log(`${instance.opts.title} WARN! requests possibly is not being processed`);
+    }
+  });
+
+  instance.on('done', (results) => {
+    console.log(`${instance.opts.title} Results:`);
+    console.log(`Avg Tput (Req/sec): ${results.requests.average}`);
+    console.log(`Avg Lat (ms): ${results.latency.average}`);
+
+    const row = `${instance.opts.title},${results.requests.average},${results.latency.average}\r\n`;
+    fs.appendFileSync(RESULT_PATH, row);
+    console.log(`AutoCannon throughput result is saved at ${RESULT_PATH}`);
+  });
+
+  // this is used to kill the instance on CTRL-C
+  process.on('SIGINT', () => {
+    console.log('\nGracefully shutting down from SIGINT (Ctrl-C)');
+
+    instance.stop();
+  });
+}
 
 function startBenchGet(path) {
   const url = 'http://localhost:3001';
@@ -35,37 +111,4 @@ function startBenchGet(path) {
 // startBenchGet('/youpi/short/alexandre');
 // startBenchGet('/youpi/passhort/alexandreyoupiohoh');
 
-function startBenchPost() {
-  const url = 'http://localhost:3001';
-
-  const numConnections = 1000;
-  const maxConnectionRequests = 1000;
-
-  function finishedBench(err, res) {
-    // console.log('Finished Bench', err, res);
-  }
-
-  const instance = autocannon(
-    {
-      url,
-      connections: numConnections,
-      duration: 10,
-      maxConnectionRequests,
-      headers: {
-        'content-type': 'application/json',
-      },
-      body: JSON.stringify({
-        url: 'http://localhost:3001/youpi/short/',
-        created_at: '{{createdAt}}',
-        modified_by: '{{modifiedBy}}',
-      }),
-    },
-    finishedBench,
-  );
-
-  autocannon.track(instance);
-}
-
-startBenchGet('/youpi/short/alexandre');
-startBenchGet('/youpi/passhort/alexandreyoupiohoh');
-startBenchPost();
+startBench('http://localhost:3001/youpi/short/');
